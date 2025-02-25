1d0cb0a7d64772e4747aed21f1068429483627a6
opilane <opilane@HP-CZC2349HV1>
2025-02-25 15:58:41 +0200

    Ülesanned yg1-5, tund 1

---
+++ tund1.py
@@ -0,0 +1,41 @@
+# from random import randint *
+from math import *
+#Ülesanne 1
+# nimi=input("mis on sinu nimi?: ")
+# vanus=int(input("Kui vana sa oled?: "))
+
+# print(f"Tere, maailm! Tervitran sind {nimi} Sa oled {vanus} aastat vana.")
+# print("Tere, maailm! Tervitran sind", nimi, "Sa oled", vanus,"aastat vana")
+# print("Tere, maailm! Tervitran sind"+nimi+"Sa oled"+str(vanus)+"aastat vana")
+
+# Ülesanne 2
+
+
+
+# vanus = 18
+# eesnimi = "Jaak"
+# pikkus = 16.5
+# kas_käib_koolis = True
+# print(f"Muutuja {vanus} on {type(vanus)} tüübi")
+# print(f"Muutuja {eesnimi} {type(eesnimi)} tüübi")
+# print(f"Muutuja {pikkus} on {type(pikkus)} tüübi")
+# print(f"Muutuja {kas_käib_koolis} on {type(kas_käib_koolis)} tüübi")
+
+# #Ülesanne 3
+
+
+
+# kommidearv=randint(1,100)
+# print(f"Laual on {kommidearv} kommid")
+# kommidvõtnud=int(input("Mitu kommi tahad ära võtta?"))
+# onjäänud=kommidearv-kommidvõtnud
+# print(f"Laual on jäänud {onjäänud} komme")
+
+#Ülesanne 4
+# ümbermõõt=int(input("Kui suur on ümbermõõt?: "))
+# läbimõõt=ümbermõõt//pi
+# print(f"läbimõõt on +f{läbimõõt}")
+
+#Ülesanne 5
+fthjstj
+
