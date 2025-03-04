7805f65736727467252804bb43e0559298733f97
opilane <opilane@HP-CZC2349HV1>
2025-03-04 15:37:51 +0200

    Работа доделана

--- Tund 1.3 Iseseisev töö Vigade otsing 1.py
+++ Tund 1.3 Iseseisev töö Vigade otsing 1.py
@@ -1,6 +1,6 @@
 from math import *
 import math
-#----------------
+#-------------
 print("Ruudu karakteristikud")
 try:
     a=int(input('Sisesta ruudu külje pikkus => '))
@@ -17,21 +17,36 @@ except:
     print("Külje suurus on vaja int formaadis sisestada!")
 #-------------
 
+#-------------
 print("Ristküliku karakteristikud")
-b=input("Sisesta ristküliku 1. külje pikkus => ")
-c=input("Sisesta ristküliku 2. külje pikkus => ")
-S=b*c
-print("Ristküliku pindala", S)
-P=2*(b+c)
-print("Ristküliku ümbermõõt", P)
-di=math.sqrt(b*2+c*2)
-print("Ristküliku diagonaal", round(di))
-print()
+try:
+    b=int(input("Sisesta ristküliku 1. külje pikkus => "))
+    c=int(input("Sisesta ristküliku 2. külje pikkus => "))
+    if b>0 and c>0:
+                S=b*c
+                print("Ristküliku pindala", S)
+                P=2*(b+c)
+                print("Ristküliku ümbermõõt", P)
+                di=math.sqrt(b*2+c*2)
+    else:
+        print("Ristküliku diagonaal", round(di))
+except:
+    print("Ristküliku 1. või 2. on vaja int formaadis sisestada!")
+#-------------
+
+
+#-------------
 print("Ringi karakteristikud")
-r=(input("Sisesta ringi raadiusi pikkus =>"))
-d=2*r
-print("Ringi läbimõõt", d)
-S=pi()*r*2
-print("Ringi pindala", round(S))
-C=2*pi()*r
-print("Ringjoone pikkus", round(C))
+try:
+    r=int(input("Sisesta ringi raadiusi pikkus =>"))
+    if r>0:
+        d=2*r
+        print("Ringi läbimõõt", d)
+        S=pi*r*2
+        print("Ringi pindala", round (S))
+        C=2*pi*r
+        print("Ringjoone pikkus", round (C))
+    else:
+        print("Külg ei saa olla <=0-ga")
+except:
+    print("Ristküliku raadius on vaja int formaadis sisestada!")
