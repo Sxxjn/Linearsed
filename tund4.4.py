8594cdd463da075a57776dab6dfbc459ae9ca0e8
opilane <opilane@HP-CZC2349HV1>
2025-03-20 11:46:55 +0200

    4.5

--- tund4.4.py
+++ tund4.4.py
@@ -1,101 +1,88 @@
 #Töö 4.4
-#1
+#1 
 from string import *
-vokaali=["a", "e", "u", "o", "i", "ü", "õ", "ä", "ö"]
-konsonanti= "qwrtpsdfghjklzxcvbnm"
+vokaali=["a","o","i","e","u","ü","õ","ä","ö"]
+konsonanti="qwrtypsdfghjklzxcvbnm"
 numbrid=digits
 märkid=punctuation
 v=k=n=m=t=0
-tekst=input("Sisend sõna või lause: ").lower()
+tekst=input("Sisend (sõna või lause): ").lower()
 tekst_list=list(tekst)
-
-for s in tekst_list:
-    if s in vokaali:
-        v+=1
-    elif s in konsonanti:
-        k+=1
-    elif s in numbrid:
-        n+=1
-    elif s in märkid:
-        m+=1
-    elif s==" ":
-        t+=1
-    print(f"Vokaali: {v} \nKonsonanti: {k} \nNumbrid: {n} \nMärkid: {m} \nTühik: {t}")
+if tekst_list.index(" ")>0:
+    print("See on lause")
+    for s in tekst_list:
+        if s in vokaali:
+            v+=1
+        elif s in konsonanti:
+            k+=1
+        elif s in numbrid:
+            n+=1
+        elif s in märkid:
+            m+=1
+        elif s==" ":
+            t+=1
+    print(f"V: {v}\nK: {k}\nN: {n}\nM: {m}\nT: {t}")
 else:
     print(f"Kokku on {len(tekst_list)}")
-    if s in vokaali:
-            v+=1
-    elif s in konsonanti:
-        k+=1
-    print(f"Vokaali: {v} \nKonsonanti: {k}")
 
-#2 
+#2
+#2.1
 nimed=[]
-for i in range(5):
-    nimi=input(f"Sisesta {i+1}. nimi: ")
+for a in range (5): #Запрос пяти имён у пользователя.
+    nimi=input("Sisesta nimi: ")
     nimed.append(nimi)
-print("Tähestikulises järjekorras:", sorted(nimed))
-print("Viimati lisatud nimi:", nimed[-1])
-
-muuda=input("Kas soovid nime muuta? (jah/ei): ")
-if muuda.lower()=="jah":
-    vana_nimi=input("Sisesta nimi, mida soovid muuta: ")
+nimed.sort() #Сортировка в порядке убывания
+print(nimed)
+print(nimed[-1]) #Отдельно вывод последнего введённого имени
+muuda=input("Kas soovite nimi muuta? jah/ei ").lower() #Спросить если хотите поменять имя.
+if muuda == "jah": #Если да, то
+    vana_nimi=input("Mis nimi te soovite muuta?: ") #Спросить какое имя заменить
     if vana_nimi in nimed:
-        uus_nimi=input("Sisesta uus nimi: ")
-        indeks=nimed.index(vana_nimi)
-        nimed[indeks]=uus_nimi
-        print("Uuendatud loend:", nimed)
+        uus_nimi=input("Sisesta uus nimi: ") #Спросить новое имя
+        ind=nimed.index(vana_nimi)
+        nimed[ind]=uus_nimi
+        nimed.sort() #Повторная сортировка по алфавиту
+        print(f"Uuendatud nimed: {nimed}") #Показать обновлённый список имён.
     else:
-        print("Nime ei leitud.")
-
-# 2.2 
-õpilased=["Juhan","Kati","Mario","Mario","Mati","Mati"]
-unikaalsed_õpilased=list(set(õpilased))
-print('Unikaalsed õpilased:', unikaalsed_õpilased)
-
-# 2.3 
-vanused=[15, 22, 17, 19, 22, 16, 18]
-print("Vanuste kogusumma:", sum(vanused))
-print("Vanuste keskmine:", sum(vanused)/len(vanused))
-
-#3 
-print("Lintdiagramm:")
-for vanus in vanused:
-    print("*" * vanus)
-
-#4 
-postiindeks=input("Sisestage postiindeks: ")
-try:
-    postiindeks=int(postiindeks)
-except ValueError:
-    print("Vigane sisend! Palun sisesta viiekohaline number.")
-    exit()
-if postiindeks<10000 or postiindeks>99999:
-    print("Vigane sisend! Postiindeks peab olema viiekohaline number.")
-    exit()
-esimene_nr=postiindeks//10000
-if esimene_nr==1:
-    maakond="Tallinn"
-elif esimene_nr==2:
-    maakond="Narva, Narva-Jõesuu"
-elif esimene_nr==3:
-    maakond="Kohtla-Järve"
-elif esimene_nr==4:
-    maakond="Ida-Virumaa, Lääne-Virumaa, Jõgevamaa"
-elif esimene_nr==5:
-    maakond="Tartu linn"
-elif esimene_nr==6:
-    maakond="Tartumaa, Põlvamaa, Võrumaa, Valgamaa"
-elif esimene_nr==7:
-    maakond="Viljandimaa, Järvamaa, Harjumaa, Raplamaa"
-elif esimene_nr==8:
-    maakond="Pärnumaa"
-elif esimene_nr==9:
-    maakond="Läänemaa, Hiiumaa, Saaremaa"
+        print("Sellist nime loendis ei ole") #Если имени нет в списке, то выводит сообщение, что такого имени там нет.
 else:
-    maakond="Tundmatu piirkond"
-teade="Оставайтесь дома!" if esimene_nr in [1, 2, 3] else "Носите маски!"
-print(f"Postiindeks {postiindeks} kuulub piirkonda: {maakond}. {teade}")
+    print("ok") #Если человек не хочет изменять имя, то написать ОК
+
+#2.2
+kord_opilased=["Juhan","Kati","Mario","Mario","Mati","Mati"] 
+print(kord_opilased) #Показывается несколько имён, среди них есть повторяющиеся.
+opilased=list(set(kord_opilased))  #Удаление дублирующихся имён
+print(opilased) #Вывод имён без повторов
+
+#2.3
+vanus=5,13,25,9,20,17,11,10,8,24
+print(vanus) #вывод возрастов
+min_vanus=min(vanus) #Показывает самый минимальный из этого списка
+print(f"\nKõige noorem on {min_vanus}")
+max_vanus=max(vanus) #максимальный возраст из списка
+print(f"Kõige vanem on {max_vanus}")
+summa=sum(vanus) #Сумма всех возрастов
+print(f"Vanuste summa on {summa}")
+keskmine=summa/len(vanus) #средний возраст
+print(f"Keskmine vanus on {keskmine}")
+
+#3
+sisend=[8,12,25,38,67,15,48] 
+for number in sisend: #Используем значения чисел в списке и создаём гистограмму, используя звезды.
+    print('*' *number)
+
+#4
+region={"1":"Tallinn","2":"Narva, Narva-Jõesuu","3":"Kohtla-Järve","4":"Ida-Virumaa, Lääne-Virumaa, Jõgevamaa","5":"Tartu linn", "6":"Tartumaa, Põlvamaa, Võrumaa, Valgamaa","7":" Viljandimaa, Järvamaa, Harjumaa, Raplamaa","8": "Pärnumaa","9":"Läänemaa, Hiiumaa, Saaremaa"} #Список со значениями, если 1 то Таллинн, если 2 то Нарва, Нарва-Йыесуу, если 3 то Кохтла-Ярве и т.д.
+post_indeks=input("Sisestage postiindeks: ") #Просит ввести индекс
+if post_indeks.isdigit() and len(post_indeks) == 5: #проверяет что состоит только из цифр и длина только 5 цифр
+    maakond=region.get(post_indeks[0]) #смотрит какой первый символ вводит пользователь и проверяет в словаре.
+    print(f"Teie postiindeks kuulub piirkonda: {maakond}") #выводит к какому региону относится индекс
+    if post_indeks[0] in ["1","2","3"]:
+        print("Olge kodus") #если индекс начинается с 1,2 или 3, то говорит остаться дома
+    else:
+        print("Kandke maske!") #на все остальные говорит носить маску
+else:
+    print("Vigane sisend!") #если такого региона нет в словаре,то ошибка ввода
 
 #5
 sisend=input("Sisestage numbrid komadega eraldatult: ")
@@ -113,6 +100,31 @@ for i in range(n):
 
 print("Muutunud list:", numbrid)
 
+#ülesanne 11
+import string
+n=int(input("Sisestage tähtede arv: ")) #Запрашиваем количество букв
+tähed=list(string.ascii_lowercase[:n]) #добавляются в список все строчные буквы английского алфавита, берётся столько символов сколько введёт пользователь
+korduvad_tähed=[] #создаём список с повторяющимися буквами
+for i in range(n):
+    korduvad_tähed.append(tähed[i]*(i+1)) #в цикле повторяется, в зависимости от позиции и добавляется в список
+print("Järjend:", tähed) #буквы которые мы используем
+print("Teine järjend:", korduvad_tähed) #те же буквы, но повторяются взависимости от позиции
+
+#12
+import random
+
+numbers=[random.randint(1, 100) for _ in range(10)]
+print("Algne nimekiri", numbers)
+
+min_value=min(numbers)
+max_value=max(numbers)
+
+min_index=numbers.index(min_value)
+max_index=numbers.index(max_value)
+
+numbers[min_index], numbers[max_index] = numbers[max_index], numbers[min_index]
+
+print("Muudetud loend", numbers)
 
 
 
