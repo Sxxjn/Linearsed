013c3d4c3d2796b4d98222929c298baff03510c2
opilane <opilane@HP-CZC2349HV1>
2025-03-27 10:00:59 +0200

    again

--- Tund5_Funktsioonide_kasutamine.py
+++ Tund5_Funktsioonide_kasutamine.py
@@ -1,55 +1,69 @@
 from Tund5_Kasutataja_fun import *
 
-#1
+# #1
+# while True:
+#     try:
+#         a=float(input("Arv1: "))
+#         break
+#     except:
+#         print("Viga! Sisesta arv")
+# while True:
+#     try:
+#         b=float(input("Arv2: "))
+#         break
+#     except:
+#         print("Viga! Sisesta arv")
+# while True:
+#     try:
+#         t=input("Tehe: ")
+#         break
+#     except:
+#         print("Viga! Sisesta (/);(*);(+);(-)")
+# vastus=arithmetic(a,b,t)
+# print(vastus)
+
+
+# #2
+# aasta=int(input("Mis aasta tahad kontrollida: "))
+# if aasta>0:
+#     v=is_year_leap(aasta)
+#     print(v)
+#     if v:
+#         print(f"{aasta} on liigasta")
+#     else:
+#         print(f"{aasta} ei ole liigasta")
+
+# #3 square() kasutaimine
+# #Kontroll while True ja try except
+# while True:
+#     try:
+#         a=float(input("Ruudu külje pikkus: "))
+#         break
+#     except:
+#         print("Viga! Sisesta arv")
+# S,P,d=square(a)
+# print(f"Ruudu pindala on {S}, ümbermõõt on {P} ja diagonaal on {d}")
+
+# #4
+# while True:
+#         try:
+#             a=int(input("Kuu number: "))
+#             break
+#         except:
+#             print("Viga! Sisesta arv")
+# vastus=season(a)
+# print(vastus)
+
+#5
 while True:
     try:
-        a=float(input("Arv1: "))
+        aasta=int(input("Sisesta aasta: "))
         break
     except:
         print("Viga! Sisesta arv")
 while True:
     try:
-        b=float(input("Arv2: "))
+        summa=int(float("Sisesta summa:"))
         break
     except:
         print("Viga! Sisesta arv")
-while True:
-    try:
-        t=input("Tehe: ")
-        break
-    except:
-        print("Viga! Sisesta (/);(*);(+);(-)")
-vastus=arithmetic(a,b,t)
-print(vastus)
-
-
-#2
-aasta=int(input("Mis aasta tahad kontrollida: "))
-if aasta>0:
-    v=is_year_leap(aasta)
-    print(v)
-    if v:
-        print(f"{aasta} on liigasta")
-    else:
-        print(f"{aasta} ei ole liigasta")
-
-#3 square() kasutaimine
-#Kontroll while True ja try except
-while True:
-    try:
-        a=float(input("Ruudu külje pikkus: "))
-        break
-    except:
-        print("Viga! Sisesta arv")
-S,P,d=square(a)
-print(f"Ruudu pindala on {S}, ümbermõõt on {P} ja diagonaal on {d}")
-
-#4
-while True:
-        try:
-            a=int(input("Kuu number: "))
-            break
-        except:
-            print("Viga! Sisesta arv")
-vastus=season(a)
-print(vastus)
